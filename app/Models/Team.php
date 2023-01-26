ea71d43fd723d5115efc19a573ef63e617ecd870
Boedoetmom <trimom806@gmail.com>
2023-01-26 14:19:28 +0700

    membuat relasi antar model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Team extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'icon',
+        'company_id',
+    ];
+
+    public function company(){
+        return $this->belongsToMany(Company::class);
+    }
+
+    public function employees(){
+        return $this->hashMany(Employee::class);
+}
 }
